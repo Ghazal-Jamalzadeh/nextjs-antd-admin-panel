eb6f51934569f37a3f0c3d4d8224fdd528447cdd
Ghazal <jamalzadeh@entekhabelectronic.com>
2024-11-04 11:25:43 +0330

    menu items

--- src/components/AppSideMenu.js
+++ src/components/AppSideMenu.js
@@ -14,9 +14,32 @@ function AppSideMenu() {
             key : 2
         } ,
         {
+            type : 'divider'
+        } ,
+        {
             label : <Link href='/courses'>Courses</Link> ,
             key : 3
-        }
+        } ,         {
+            label : <Link href='/tutorials'>Tutorials</Link> ,
+            key : 4
+        } ,         {
+            label : <Link href='/bestPractices'>Best Practices</Link> ,
+            key : 5
+        } ,         {
+            label : <Link href='/certifications'>Certifications</Link> ,
+            key : 6
+        } ,
+        {type : "divider"} ,
+        {
+            label : <Link href='/resources'>Resources</Link> ,
+            key : 7
+        } ,         {
+            label : <Link href='/events'>Events</Link> ,
+            key : 8
+        } ,         {
+            label : <Link href='/community'>Community</Link> ,
+            key : 9
+        } ,
     ]
 
     return (
